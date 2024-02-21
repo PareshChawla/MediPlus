7b05a2184101f0905a38c967c672fea794251ffa
Paresh Chawla <pareshchawla.56@gmail.com>
2024-02-21 10:27:34 +0530

    Change height in card fulfillment

--- src/components/CardFullfillment.js
+++ src/components/CardFullfillment.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CardFullfillment = ({logo, title, content}) => {
   return (
-    <div className='border-2 border-[#6CB250] hover:border-[#3470a1] w-[100%] lg:w-[370px] h-80 rounded-xl flex flex-col shadow-lg hover:shadow-xl hover:shadow-blue-500/10  items-center'>
+    <div className='border-2 border-[#6CB250] hover:border-[#3470a1] w-[100%] lg:w-[370px] min-h-full lg:h-80 rounded-xl flex flex-col shadow-lg hover:shadow-xl hover:shadow-blue-500/10  items-center'>
     <div className='p-8'>{logo}</div>
     <div>
         <h1 className='text-center text-[#3470a1] font-bold text-2xl p-5'>{title}</h1>
--- src/components/MedicineCarousel.js
+++ src/components/MedicineCarousel.js
@@ -3,7 +3,7 @@ import React from 'react'
 const MedicineCarousel = ({img, medName}) => {
   return (
     <div className='flex flex-col items-center justify-center gap-2'>
-        <div className='rounded-full lg:w-32 w-24 lg:h-36'>
+        <div className='rounded-full lg:w-32 w-16 lg:h-36'>
             <img className='rounded-full' src={img} alt="meds" />
         </div>
         <h1 className='font-bold text-[#6cb250] my-3 text-center'>{medName}</h1>
